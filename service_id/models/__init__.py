4750d5fa64655d025cba8042f47a64d083f27666
yiannis_stavrou <50438663+LGodoo@users.noreply.github.com>
2022-10-14 18:20:22 +0300

    Add files via upload

---
+++ service_id/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from . import sale_subscription_line
+from . import sale_subscription
+from . import account_move_line
+from . import sale_service_details
+from . import product_template
